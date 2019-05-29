9c1a520268bf3884893c5b8b6107fc212f1cac9b
Will Morrissey <wtm.41691@gmail.com>
2019-05-29 16:41:26 -0400

    Small update on itemlist functions that connect to service

--- Public/components/itemlist.js
+++ Public/components/itemlist.js
@@ -1,1 +1,41 @@
 // front-end functionality and angular template here 
+
+function itemList(CartService) {
+
+    const ctrl = this; 
+
+    ctrl.addItem = (newProduct, newPrice, newQuantity) => {
+    
+        let newItem = {
+          product: newProduct,
+          price: newPrice,
+          quantity: newQuantity,
+        } 
+
+        CartService.addItem(newItem)
+        .then( (data) => {
+          ctrl.cartData = data;
+        })
+      }
+
+
+      ctrl.removeItem = (id) => {
+        CartService.removeItem(id)
+        .then( (data) => {
+          ctrl.cartData = data;
+        })
+      }
+
+}
+
+angular 
+.module('CartApp')
+.component(itemList, {
+    controller: itemList,
+    template: 
+    `
+        <div class = "container">
+
+        </div>
+    `
+})
